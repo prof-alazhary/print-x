d4ce2343e86b9c50fa8e5c28ca3acdcd79dc8eae
Mohammed Al-Azhary <mohammad.alazhary@enichers.com>
2019-06-27 23:56:29 +0200

    add User model and Litter model

---
+++ models/Litter.js
@@ -0,0 +1,41 @@
+const { model, Schema } = require('../config/mongoose'),
+    promisify = require('../helpers/promisify');
+
+const litterSchema = new Schema({
+    user:{
+        type: String,
+        required: true
+    },
+    destination:{
+        type: String,
+        default: ''
+    },
+    machineData:{
+        type: Object,
+        default:{
+            model:'',
+            chassisNo:'',
+            motorNo:'',
+            manufactureYear:'',
+            color:''
+
+        }
+    },
+    customerData:{
+        type: Object,
+        default:{
+            name:'',
+            nationalId:'',
+            address:'',
+            city:''
+        }
+    }
+});
+
+litterSchema.index({'machineData.model':'text'});
+
+const Litter = model('Litter', litterSchema);
+console.log('Litter--->',Litter)
+promisify(Litter,['updateOne','update','find', 'findOne','create','deleteOne']);
+
+module.exports = Litter;
---
+++ models/User.js
@@ -0,0 +1,17 @@
+const { model, Schema } = require('../config/mongoose'),
+    promisify = require('../helpers/promisify');
+
+const userSchema = new Schema({
+    email:{
+        type: email
+    },
+    password:{
+        type: password
+    }
+});
+
+const User = model('User', userSchema);
+
+promisify(User,['updateOne','update','find', 'findOne','create','deleteOne']);
+
+module.exports = User;
