a04938ec2083716b1699dc3cb8e1a30f9d11fe48
Mohammed Al-Azhary <mohammad.alazhary@enichers.com>
2019-07-27 02:22:36 +0200

    refactor position of pdf fields

--- services/LitterService.js
+++ services/LitterService.js
@@ -8,10 +8,9 @@ module.exports = {
         return Litter.findOne({
             $or: [
                 { 'machineData.motorNo': motorNo },
-                { 'machineData.chassisNo': chassisNo },
+                { 'machineData.chassisNo': chassisNo }
             ]
         }).then(_litter => {
-
             if (!_litter) {
                 return Litter.create(litter).then(litter => {
                     return this.select(litter.id);
@@ -25,16 +24,16 @@ module.exports = {
             }
         });
     },
-    edit(litterId){
-        return Litter.findOne({ _id: ObjectId(litterId) })
+    edit(litterId) {
+        return Litter.findOne({ _id: ObjectId(litterId) });
     },
     update(litterId, litter) {
         return Litter.updateOne(
             { _id: ObjectId(litterId) },
             {
                 $set: litter
-            })
-            .then(()=>this.select(litterId));
+            }
+        ).then(() => this.select(litterId));
     },
     select(litterId) {
         return Litter.findOne({ _id: ObjectId(litterId) }).then(litter => {
@@ -44,12 +43,12 @@ module.exports = {
     delete(litterId) {
         return Litter.deleteOne({ _id: ObjectId(litterId) });
     },
-    search(criteria={}) {
+    search(criteria = {}) {
         const { chassisNo, motorNo, dealer } = criteria;
 
         if (dealer) {
             criteria = {
-                'dealer': {
+                dealer: {
                     $regex: new RegExp(`^${dealer}.*`)
                 }
             };
@@ -71,16 +70,10 @@ function testPDFkit(data) {
     const doc = new PDFDocument({
         //layout: 'landscape', // or 'letter' ....
         size: [612, 820] // [doc.page.width, doc.page.height]
-      });
-    
+    });
+
     const { destination, machineData, customerData } = data,
-        {
-            model,
-            chassisNo,
-            motorNo,
-            manufactureYear,
-            color
-        } = machineData,
+        { model, chassisNo, motorNo, manufactureYear, color } = machineData,
         { name, nationalId, address, city } = customerData;
 
     let { dealer } = data;
@@ -97,37 +90,56 @@ function testPDFkit(data) {
 
     doc.text(destination, 350, 240);
 
-    doc.text(destination, 145, 390);
+    doc.text(destination, 145, 390 - 5);
 
-    doc.text(model, 390, 360);
+    doc.font('Times-Bold');
+    doc.fontSize(18);
 
-    doc.text(chassisNo, 320, 390);
+    doc.text(model, 390, 360 + 10);
 
-    doc.text(motorNo, 370, 420);
+    doc.text(chassisNo, 315, 390 + 10);
 
-    doc.text(manufactureYear , 390, 450);
+    doc.text(motorNo, 370, 420 + 10);
 
-    doc.text(color, 390, 480);
+    doc.text(manufactureYear, 390, 450 + 10);
 
-    let nameX = 280;
-    if(dealer.split(' ').length > 3){
-        nameX = 250;
+    doc.text(color, 390, 480) + 10;
+
+    doc.font('public/fonts/Scheherazade-Regular.ttf');
+    doc.fontSize(20);
+
+    let nameX = 0;
+    const words = name.split(' ');
+
+    switch (words.length) {
+        case 5:
+            nameX = 280;
+            break;
+        case 4:
+            nameX = 300;
+            break;
+        case 3:
+            nameX = 310;
+            break;
+        default:
+        nameX = 260;
+            break;
     }
+
     doc.text(rtlText(name), nameX, 540);
 
     doc.text(nationalId, 60, 540);
 
-    doc.text(rtlText(address), 440, 570);
+    doc.text(rtlText(address), 420, 570);
 
     doc.text(city, 100, 570);
-    
+
     doc.fontSize(17);
     let dealerX = 510;
-    if(dealer.split(' ').length > 1){
-        dealerX = 485
+    if (dealer.split(' ').length > 1) {
+        dealerX = 485;
         dealer = rtlText(dealer);
     }
-    console.log('--->',doc.page.height,doc.page.width)
     //This is a footer
     doc.text(dealer, dealerX, doc.page.height - 30, {
         lineBreak: false
@@ -137,7 +149,7 @@ function testPDFkit(data) {
 }
 
 function rtlText(text) {
-    text =" "+text+" ";
+    text = ' ' + text + ' ';
     return text
         .split(' ')
         .reverse()
